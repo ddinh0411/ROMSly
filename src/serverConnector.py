9e238704f8aef7ad48ca378cf2fbc0d16dabd9f1
masli747 <masli@chapman.edu>
2023-12-07 05:20:30 -0800

    Made comments in serverConnector and index more detailed.

--- src/serverConnector.py
+++ src/serverConnector.py
@@ -6,21 +6,25 @@ import pandas as pd
 app = Flask(__name__)
 
 def get_db_connection():
+    # Connect to your MySQL server. 
+    # This is currently a localhost instance, so you will need to provide your own.
     return pymysql.connect(host='127.0.0.1', port=3306, user='root', password='ROMSly', db='mysql')
 
 @app.route('/')
 def index():
+    # Display the Blockly UI.
     return render_template('index.html')
 
 @app.route('/query', methods=['POST'])
 def query():
+    # Get the Blockly generated Python code from index.html
     data = request.get_json()
     js_variable = data.get('variable', '')
 
+    # Dynamically execute our generated Blockly code.
     exec(js_variable)
     
-    # Do something with the JavaScript variable
-    print("Received JavaScript variable:", js_variable)
+    # print("Received JavaScript variable:", js_variable)
 
     # Send a response back to the client with redirect instruction
     response_data = {'message': 'Variable received successfully', 'redirect': url_for('view')}
@@ -28,6 +32,7 @@ def query():
 
 @app.route('/view')
 def view():
+    # data contains a SQL Query to join together a single orders table for display.
     data = """SELECT distinct
     o.OrderId,
     t.FoodItems,
@@ -56,11 +61,15 @@ LEFT JOIN (
     GROUP BY o.orderId
 ) AS t2 ON t2.OrderId = o.OrderId
 ORDER BY o.OrderId;"""
-    # Connect to MySQL and execute some query
+
+    # Connect to MySQL database and execute the query to present all orders. 
     connection = get_db_connection()
+    # Read the query into a panda.
     df = pd.read_sql(data, connection)
+    # Make the orderID's the panda index for better organization.
     df = df.set_index('OrderId')
     
+    # Display it to our user.
     return render_template('results.html',  tables=[df.to_html(classes='data', header="true")])
 
 if __name__ == '__main__':
